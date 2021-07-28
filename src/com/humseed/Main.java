0610c4b45d8ca601c187b49b241ad6b60a43a2f0
leozbshi <5361232+leozbshi@users.noreply.github.com>
2021-07-28 09:25:19 +0800

    git init

---
+++ src/com/humseed/Main.java
@@ -0,0 +1,9 @@
+package com.humseed;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("004");
+    }
+}
